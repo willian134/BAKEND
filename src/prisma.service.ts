c2747e97dc5806b3c6062a06d25297f217407def
willianorellana830-cell <weorellana@sudamericano.edu.ec>
2026-07-26 23:42:31 -0500

    Fix prisma.service.ts for Prisma 5

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,18 +1,9 @@
-import { Injectable, OnModuleInit } from '@nestjs/common'
-import { PrismaClient } from '@prisma/client'
-import { PrismaPg } from '@prisma/adapter-pg'
-
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
+﻿import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({ adapter })
-  }
-
   async onModuleInit() {
-    await this.$connect()
+    await this.$connect();
   }
 }
